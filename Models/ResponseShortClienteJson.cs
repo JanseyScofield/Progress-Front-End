35801a2cff6a70f4c673e7f410526203dae3db78
JanseyScofield <janscofield10@gmail.com>
2025-04-09 16:13:36 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ResponseShortClienteJson.cs
@@ -0,0 +1,12 @@
+﻿namespace ProgressFrontEnd.Models
+{
+    public class ResponseShortClientesJson
+    {
+        public string CNPJ { get; set; }
+        public string Endereco { get; set; }
+        public string RazaoSocial { get; set; }
+        public string NomeFantasia { get; set; }
+        public string Telefone { get; set; }
+        public DateTime ProximaVisita { get; set; }
+    }
+}
